d6f7b5c39309aeaee777be96c51e571bda787402
Shaina <143354273+shainabanerjee@users.noreply.github.com>
2026-08-22 19:03:05 -0400

    adding hotel to db

--- project/server.js
+++ project/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path');
+const session = require('express-session');
 let hostname = "localhost";
 
 const foodRouter = require('./routes/food-server');
@@ -13,6 +14,17 @@ app.use(express.static(path.join(__dirname, 'app','public')));
 app.use(express.static(path.join(__dirname, 'app','public','html')));
 app.use(express.json());
 
+app.use(session({
+    secret: process.env.SESSION_SECRET || 'development_fallback_secret_key',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        httpOnly: true,
+        secure: false,
+        sameSite: "strict",
+        maxAge: 1000 * 60 * 60 * 24
+    }
+}));
 
 app.use('/', foodRouter);
 app.use('/', flightsRouter);
