bd23ba6e2e465c842ff29d8fb3ef7116a1d03eb8
Ruby Xu <139299636+lWhiteNightl@users.noreply.github.com>
2026-08-27 16:07:28 -0400

    edit localhost main page link in console for easier access

--- project/server.js
+++ project/server.js
@@ -39,7 +39,7 @@ app.use('/', friendRouter);
 app.use('/', tripRouter);
 
 const server = app.listen(port, hostname, () => {
-    console.log(`http://${hostname}:${port}/login.html`);
+    console.log(`http://localhost:${port}/index.html`);
 })
 
 const { addVotingServer } = require('./clientHandler');
